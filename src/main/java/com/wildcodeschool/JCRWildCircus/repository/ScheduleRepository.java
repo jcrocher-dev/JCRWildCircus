6be12bf95bb67827970d026b6d21482cba7b0d41
Jean-christophe Rocher <jcrocher@orange.fr>
2020-01-29 18:14:22 +0100

    create repo

---
+++ src/main/java/com/wildcodeschool/JCRWildCircus/repository/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package com.wildcodeschool.JCRWildCircus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wildcodeschool.JCRWildCircus.entities.Schedule;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>{
+
+}
