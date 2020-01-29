82fb1dba7a9502466357f7c47ad7ccdc5ebb5030
Jean-christophe Rocher <jcrocher@orange.fr>
2020-01-29 10:59:46 +0100

    firstcommit

---
+++ src/main/java/com/wildcodeschool/JCRWildCircus/JcrWildCircusApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.JCRWildCircus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcrWildCircusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcrWildCircusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/JCRWildCircus/JcrWildCircusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.JCRWildCircus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcrWildCircusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
